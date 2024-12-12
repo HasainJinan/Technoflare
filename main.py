e593c12823b0b334577e7d1ae8a522d519802aa0
Hasain Jinan <hjinan@brooklynsteamcenter.org>
2024-12-12 10:53:51 -0500

    Added Functioning Product Page.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def product_browse():
         cursor.execute("SELECT * FROM `Product`;")
 
     else:
-        cursor.execute(f"SELECT * FROM `Product` WHERE `name` LIKE '%{query}%' OR `description` LIKE '%{query}%' OR `price` LIKE '%{query}%';")
+        cursor.execute(f"SELECT * FROM `Product` WHERE `name` LIKE '%{query}%' OR `description` LIKE '%{query}%' OR `price` LIKE '%{query}%' OR `type` LIKE '%{query}%';")
 
     results = cursor.fetchall()
 
@@ -45,3 +45,18 @@ def product_browse():
     conn.close()
 
     return render_template("browse.html.jinja", products = results)
+
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Product` WHERE `id` = {product_id};")
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("product.html.jinja", product = result)
